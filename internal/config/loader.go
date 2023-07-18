f75ae94ec714de3980c018d5de01373e98528a03
mhdianrush <mhdianrush@gmail.com>
2023-07-18 14:06:46 +0700

    Created for Get Function

---
+++ internal/config/loader.go
@@ -0,0 +1,33 @@
+package config
+
+import (
+	"os"
+
+	"github.com/joho/godotenv"
+	"github.com/sirupsen/logrus"
+)
+
+var logger = logrus.New()
+
+func Get() *Config {
+	// env on root
+	err := godotenv.Load()
+
+	if err != nil {
+		logger.Printf("error load env %s", err.Error())
+	}
+
+	return &Config{
+		Server: Server{
+			Host: os.Getenv("SERVER_HOST"),
+			Port: os.Getenv("SERVER_PORT"),
+		},
+		Database: Database{
+			Host:     os.Getenv("DATABASE_HOST"),
+			Port:     os.Getenv("DATABASE_PORT"),
+			User:     os.Getenv("DATABASE_USER"),
+			Password: os.Getenv("DATABASE_PASSWORD"),
+			Name:     os.Getenv("DATABASE_NAME"),
+		},
+	}
+}
