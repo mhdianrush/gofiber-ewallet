3aed15bc12064e20a124f8db5467ca3eae5de2cc
mhdianrush <mhdianrush@gmail.com>
2023-07-19 12:38:13 +0700

    Developed for Authenticate and ValidateToken Function

---
+++ internal/service/user.go
@@ -0,0 +1,61 @@
+package service
+
+import (
+	"context"
+	"encoding/json"
+	"gofiber-ewallet/domain"
+	"gofiber-ewallet/dto"
+	"gofiber-ewallet/internal/util"
+
+	"golang.org/x/crypto/bcrypt"
+)
+
+type userService struct {
+	userRepository  domain.UserRepository
+	cacheRepository domain.CacheRepository
+}
+
+func NewUser(userRepository domain.UserRepository) domain.UserService {
+	return &userService{
+		userRepository: userRepository,
+	}
+}
+
+func (u userService) Authenticate(ctx context.Context, req dto.AuthReq) (dto.AuthRes, error) {
+	user, err := u.userRepository.FindByUsername(ctx, req.Username)
+	if err != nil {
+		return dto.AuthRes{}, err
+	}
+	if user == (domain.User{}) {
+		return dto.AuthRes{}, domain.ErrAuthFailed
+	}
+	err = bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(req.Password))
+	if err != nil {
+		return dto.AuthRes{}, domain.ErrAuthFailed
+	}
+	// generate token
+	token := util.GenerateRandomString(7)
+	userJson, _ := json.Marshal(user)
+	_ = u.cacheRepository.Set("user: "+token, userJson)
+
+	return dto.AuthRes{
+		Token: token,
+	}, nil
+}
+
+func (u userService) ValidateToken(ctx context.Context, token string) (dto.UserData, error) {
+	data, err := u.cacheRepository.Get("user: " + token)
+	if err != nil {
+		return dto.UserData{}, domain.ErrAuthFailed
+	}
+
+	var user domain.User
+	_ = json.Unmarshal(data, &user)
+
+	return dto.UserData{
+		Id:       user.Id,
+		FullName: user.FullName,
+		Phone:    user.Phone,
+		Username: user.Username,
+	}, nil
+}
