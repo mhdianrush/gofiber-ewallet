688b87a1e4dbb5cb9e8b4275831aba979ff3530e
mhdianrush <mhdianrush@gmail.com>
2023-07-19 14:52:53 +0700

    Implemented Config, Database Connection, userRepository, and userService

--- main.go
+++ main.go
@@ -1,12 +1,23 @@
 package main
 
 import (
+	"gofiber-ewallet/internal/component"
+	"gofiber-ewallet/internal/config"
+	"gofiber-ewallet/internal/repository"
+	"gofiber-ewallet/internal/service"
 	"os"
 
 	"github.com/sirupsen/logrus"
 )
 
 func main() {
+	config := config.Get()
+	db := component.ConnectDB(config)
+	cacheConnection := component.GetCacheConnnection()
+	userRepository := repository.NewUser(db)
+	userService := service.NewUser(userRepository, cacheConnection)
+	
+
 	logger := logrus.New()
 
 	file, err := os.OpenFile("application.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
